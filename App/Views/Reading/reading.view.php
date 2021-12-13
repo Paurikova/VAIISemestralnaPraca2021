d9332be8784c147a705af12b632812612c6869cb
Paurikova <miska.paurikova@gmail.com>
2021-12-13 15:52:57 +0100

    Ajax work

--- App/Views/Reading/reading.view.php
+++ App/Views/Reading/reading.view.php
@@ -2,14 +2,11 @@
 <div class="row webside">
     <div class="col-5 col-sm-5 col-md-3 col-lg-2">
         <div class="row myInput">
-            <form class="newReading" method="post" action="?c=reading&a=addReading">
-                <div class="col-12 col-sm-12 col-md-4 col-lg-4">
-                    <input class="myTime" id="date" type="date" name="date" value="2021-01-01">
-                    <input class="myTime" id="time" type="time" name="time" min="00:00" max="23:59" required>
-                    <!--<button class="btn btn-primary btn-md myTime">Add</button>-->
-                    <button id="newReading" class="btn btn-primary btn-md myTime">Add</button>
-                </div>
-            </form>
+            <div class="col-12 col-sm-12 col-md-4 col-lg-4">
+                <input class="myTime" id="date" type="date" name="date" value="2021-01-01">
+                <input class="myTime" id="time" type="time" name="time" min="00:00" max="23:59" required>
+                <button id="newReading" class="btn btn-primary btn-md myTime">Add</button>
+            </div>
         </div>
     </div>
     <div class="col-12 col-sm-12 col-md-10 col-lg-10">
